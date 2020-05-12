a780489d6b140ea269490d9fedd87533efd23b1a
Stephanie Viveros <sviveros05@gmail.com>
2020-05-12 00:15:20 -0500

    Add value to searchbar

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, jsonify, redirect, request, session
 
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure to use SQLite database
+# db = SQL("sqlite:///books.db")
 
 @app.route('/')
 def index():
